a8cf1a1877be20ad00843d5004779a82c21220b3
Muaz Abdool <Muaz.Abdool@redpandasoftware.co.za>
2024-03-14 08:49:38 +0200

    Calculator project

--- Day_10_calculator/main.py
+++ Day_10_calculator/main.py
@@ -1,41 +1,74 @@
 import  art
 print(art.logo)
-calculator = {}
-#input
-# number
-first_num = int(input('whats the first number'))
-print('+')
-print('-')
-print('*')
-print('/')
-# +
-# -
-# *
-# /
 
-operation = input('Pick an operation :')
-# Whats the next number :?
-second_num = int(input("What's the next number?:"))
-# 5/2 = 2. 5
-# Type 'y' to continue calculating with the above result(2.5) , or type 'n' to start a new calculation
+def pickOperation():
+    print('+')
+    print('-')
+    print('*')
+    print('/')
+    operation = input('Pick an operation :')
+    return  operation
 
 def performOperation(num1 ,num2,operation):
-    total=0
     match operation:
         case '+':
-            total = num1 +num2
+            return num1 + num2
         case '-':
-            total = num1 - num2
+            return num1 - num2
         case '*':
-            total = num1 * num2
+            return  num1 * num2
         case '/':
-            total = num1/num2
-    return  total
+            return   num1/num2
 
-next_operation = input("# Type 'y' to continue calculating with the above result(2.5) , or type 'n' to start a new calculation").casefold()
+first_num = int(input('whats the first number: ? '))
+operation = pickOperation()
+second_num = int(input("What's the next number?:"))
 
-next_operation =next_operation.casefold()
-match next_operation :
-    case 'n':
-        first_num =0
-        second_num = 0
+
+
+
+
+
+
+
+result = performOperation(num1=first_num,num2=second_num,operation=operation)
+print(f"{first_num}{operation}{second_num} = {performOperation(num1=first_num,num2=second_num,operation=operation)} ")
+
+
+while True:
+    next_operation = input(
+        f"# Type 'y' to continue calculating with the above result({result}) , or type 'n' to start a new calculation").casefold()
+
+    next_operation = next_operation.casefold()
+    match next_operation:
+        case 'n':
+
+            first_num = int(input('whats the first number: ? '))
+            operation = pickOperation()
+            second_num = int(input("What's the next number?:"))
+
+            print(
+                f"{first_num}{operation}{second_num}= {performOperation(num1=first_num, num2=second_num, operation=operation)}")
+        case 'y':
+            second_num = int(input("What's the next number?:"))
+            operation = pickOperation()
+            result2 = performOperation(num1=result, num2=second_num, operation=operation)
+
+            print(
+                f"{result}{operation}{second_num}= {result2}")
+            result = result2
+        case _:
+            break
+
+
+
+
+
+
+
+
+
+
+
+
+
