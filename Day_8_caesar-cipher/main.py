271db1208c3cff28537a5c1d76eea0ecd02376e5
Muazzi <mrawenxa@gmail.com>
2024-03-27 04:05:17 +0200

    updated the code

--- Day_8_caesar-cipher/main.py
+++ Day_8_caesar-cipher/main.py
@@ -1,4 +1,5 @@
-
+import  art
+in_caesar = False
 def caesar(direction,text,shifts ):
     new_word = []
     x = direction.casefold()
@@ -17,24 +18,23 @@ def caesar(direction,text,shifts ):
             print(f"The decoded text is {encoded_text}")
 
 
-while True:
-
-print( art.logo)
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(art.logo)
+while not in_caesar:
 
 
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt , or type exit to quit\n")
+    print(direction)
+    if direction.casefold() == 'exit':
+        in_caesar = True
+        break
 
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-
-
-
-
-
-
-caesar(direction,text,shift)
+    caesar(direction,text,shift)
+    in_caesar_prompt = input("Type 'yes' if you want to go again. Otherwise type 'no'")
+    if in_caesar_prompt =='no':
+        in_caesar = True
 
 
 
