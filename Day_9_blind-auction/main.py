888b67048add521103ca99d7ac98e9efd2a7b4b0
Muazzi <mrawenxa@gmail.com>
2024-03-27 04:51:41 +0200

    Made fixes

--- Day_9_blind-auction/main.py
+++ Day_9_blind-auction/main.py
@@ -1,27 +1,41 @@
 import locale
 import  art
-import  os
+import sys,subprocess
+import decimal
+from decimal import Decimal
+
 clear = lambda : os.system('cls')
 loc = locale.setlocale(locale.LC_ALL, '')
 conv = locale.localeconv()
 print(art.logo)
 print("Welcome to the secret auction program. ")
 bidders = {}
-name = input('What is your name?:')
-bid = float(input(f"What's your bid?:{conv['currency_symbol']}  "))
-bidders[name] =bid
+in_auction= False
+#name = input('What is your name?:')
+#bid = float(input(f"What's your bid?:{conv['currency_symbol']}  "))
+#bidders[name] =bid
 ans = ''.casefold()
-ans = input("Are there any other bidders? Type 'yes' or 'no'.")
-print("\033[H\033[3J", end="")
 
-while ans != 'no':
+#os.system('cls')
+
+while not in_auction:
     name = input('What is your name?')
-    bid = float(input(f"What's your bid?:{conv['currency_symbol']}  "))
+    bid = Decimal(input(f"What's your bid?:{conv['currency_symbol']}  "))
     bidders[name] = bid
-clear()
+    ans = input("Are there any other bidders? Type 'yes' or 'no'.")
+    if ans == 'no':
+        subprocess.run('cls', shell=True)
+        in_auction = True
+
+    if ans =='yes':
+       subprocess.run('cls',shell = True)
+
+
+
 highest_bidderName = max(bidders.keys(),key = bidders.get)
+highest_bidder_value = round(bidders[highest_bidderName],2)
 
 
 
-print(f"The winner is {highest_bidderName} with a bid of {conv['currency_symbol']} {bidders[highest_bidderName]}")
+print(f"The winner is {highest_bidderName} with a bid of {conv['currency_symbol']} {highest_bidder_value}")
 
